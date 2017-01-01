3396a6ba2c63942addea1f418881bd4666a69229
Aralya Phinith <aphinith84@gmail.com>
2017-01-01 11:20:46 -0800

    create functionality to add notes, check for duplicates, and refactoring to use reusable functions

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,35 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  };
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  console.log('Adding note: ', title, body);
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicatNotes = notes.filter((note) => note.title === title);
+
+  if (duplicatNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+  } else {
+    console.log('Title already exists!');
+  }
+
 };
 
 var getAll = () => {
