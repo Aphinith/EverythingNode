142f4261ee52ec73cbb6e2a3849dea1ae56153ec
Aralya Phinith <aphinith84@gmail.com>
2017-01-14 12:35:03 -0800

    create separate folder and file for function

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode.js');
 
 const argv = yargs
   .options({
@@ -14,21 +15,5 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-let address = encodeURI(argv.a);
-// console.log('address: ', address);
-// console.log(argv.a);
+geocode.geocodeAddress(argv.a);
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google Services.');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  } else if (body.status === 'OK') {  
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  };
-});
