8953e90884712d5258f9f66acde688b723c35b9c
Aralya Phinith <aphinith84@gmail.com>
2017-01-14 00:35:45 -0800

    execute request command to retrieve data from googlemapsapi

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,1 +1,8 @@
 const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=609%20oak%20street%20oakland',
+  json: true
+}, (error, response, body) => {
+  console.log('body: ', JSON.stringify(body, undefined, 2));
+})
