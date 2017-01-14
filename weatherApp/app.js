bb843424478c75db752eca88ffc5965b87d20daa
Aralya Phinith <aphinith84@gmail.com>
2017-01-14 12:20:45 -0800

    create checks for errors and type of response

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -14,11 +14,21 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-console.log(argv);
+let address = encodeURI(argv.a);
+// console.log('address: ', address);
+// console.log(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=609%20oak%20street%20oakland',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (error, response, body) => {
-  console.log('body: ', JSON.stringify(body, undefined, 2));
+  if (error) {
+    console.log('Unable to connect to Google Services.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  } else if (body.status === 'OK') {  
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  };
 });
