031f5c3fdf28b6501f330165a60c3e42ed01fb3f
Aralya Phinith <aphinith84@gmail.com>
2017-01-14 11:56:38 -0800

    configure yargs

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=609%20oak%20street%20oakland',
   json: true
 }, (error, response, body) => {
   console.log('body: ', JSON.stringify(body, undefined, 2));
-})
+});
